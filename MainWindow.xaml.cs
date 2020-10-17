cd00520824d95a0ee87bc7b9ddaa0ce782608b9c
VASILE-IONUȚ-REMUS IGA <vasile.iga@stud.ubbcluj.ro>
2020-10-17 18:46:50 +0300

    Redimensionat casutele

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,11 +18,19 @@ namespace Iga_VasileIonutRemus_Lab2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+
         }
     }
 }
